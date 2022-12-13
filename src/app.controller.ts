7df3908046d15b08089261190fa48fa9cf6b825a
leobraboo <leonardo.l.souza@ufv.br>
2022-12-12 23:06:50 -0300

    FEAT : service prisma + insomnia

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+
+//gerador de id universal
+import { randomUUID } from 'node:crypto';
 
 @Controller('notification')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello() {
+  list() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'um texto qualquer!!',
+        category: 'categoria',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
