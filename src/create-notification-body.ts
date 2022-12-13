478f2f32e6b72a51aad1162b5b446212eaa159ed
leobraboo <leonardo.l.souza@ufv.br>
2022-12-12 23:51:47 -0300

    REFACTOR : others validators

--- src/create-notification-body.ts
+++ src/create-notification-body.ts
@@ -1,8 +1,14 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationBody {
   @IsNotEmpty()
+  @IsUUID()
   recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 300)
   content: string;
+
+  @IsNotEmpty()
   category: string;
 }
