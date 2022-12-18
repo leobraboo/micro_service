61841a036791a7ce750fe0de20f7e0acd5b3697f
leobraboo <leonardo.l.souza@ufv.br>
2022-12-18 01:00:18 -0300

    feat repositories

--- src/application/use-cases/send-notifcation.spec.ts
+++ src/application/use-cases/send-notifcation.spec.ts
@@ -1,8 +1,15 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    console.log(notification);
+  },
+};
 
 describe('send notification', () => {
   it('deve ser possivel enviar a notificação', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'isso é uma notificação',
